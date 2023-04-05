1f818494e7233495a75763508b0bfbc0c00f8565
jwillie-26 <105637783+jwillie-26@users.noreply.github.com>
2023-04-05 15:43:12 +0300

    migrations

---
+++ db/schema.rb
@@ -0,0 +1,46 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2020_10_16_140355) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "favorites", force: :cascade do |t|
+    t.bigint "job_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["job_id"], name: "index_favorites_on_job_id"
+    t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
+
+  create_table "jobs", force: :cascade do |t|
+    t.string "company"
+    t.string "position"
+    t.string "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "salary"
+    t.string "location"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "favorites", "jobs"
+  add_foreign_key "favorites", "users"
+end
---
+++ db/seeds.rb
@@ -0,0 +1,90 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+Job.create(
+      company: 'Internal Platforms',
+      position: 'Director, Customer Advocate',
+      location: 'Remote',
+      salary: '$185,000 - $243,000 a year',
+      description: 'You will be a problem solver, using your thorough knowledge of our business and technology to connect the dots between what the WHY of your customers’ goals to…'
+  )
+
+  Job.create(
+      company: 'Microsoft',
+      position: 'Engineer',
+      location: 'Remote',
+      salary: '$185,000 - $243,000 a year',
+      description: 'You will be a problem solver, using your thorough knowledge of our business and technology to connect the dots between what the WHY of your customers’ goals to…'
+  )
+
+  Job.create(
+      company: 'Aiteq',
+      position: 'Fullstack Developer (TypeScript)',
+      location: 'Remote',
+      salary: '$185,000 - $243,000 a year',
+      description: 'You will be a problem solver, using your thorough knowledge of our business and technology to connect the dots between what the WHY of your customers’ goals to…'
+  )
+
+  Job.create(
+      company: 'SWK Technologies, Inc',
+      position: 'Developer',
+      location: 'Remote',
+      salary: '$5,500 - $7,500 a month',
+      description: 'Analyzes the needs of the user/business providing technical support for the Information Systems department, such as coding, designing, implementing and debugging programs...'
+  )
+
+  Job.create(
+      company: 'Hollstadt & Associates',
+      position: 'Java Developer',
+      location: 'Remote',
+      salary: '$185,000 - $243,000 a year',
+      description: 'Client is looking for a Java Developer to come in a and be able to provide some technical leadership to their team. They are migrating off a...'
+  )
+
+  Job.create(
+      company: 'Phrasee',
+      position: 'Python Developer',
+      location: 'Remote',
+      salary: '$185,000 - $243,000 a year',
+      description: 'We are looking for a new member of the Dev team to work alongside the existing Data Science and Engineering teams. You will responsible for improving core components…'
+  )
+
+
+  Job.create(
+      company: 'ACS Group',
+      position: 'Fullstack Java Developer',
+      location: 'Remote',
+      salary: '$185,000 - $243,000 a year',
+      description: 'AIC has an opening for a mid-level Java resource with Spring Boot & Kafka experience. Java and Spring Boot are the primary skills. If not Kafka…'
+  )
+
+  Job.create(
+      company: 'Capgemini',
+      position: 'Software Developer - Mobile Development L2',
+      location: 'Remote',
+      salary: '$185,000 - $243,000 a year',
+      description: 'Designing and developing advanced applications for the Android platform. Collaborating with the design team to define app features…'
+  )
+
+
+  Job.create(
+      company: 'Stackbuilders',
+      position: 'Front-End JavaScript Developer',
+      location: 'Remote',
+      salary: '$185,000 - $243,000 a year',
+      description: 'For this position we are looking for a JavaScript lover, passionate for front-end work that shares our enthusiasm for quality code. If you feel you…'
+  )
+
+  Job.create(
+      company: 'Ettitude Holdings, Inc.',
+      position: 'Wordpress Blog Developer',
+      location: 'Remote',
+      salary: '$185,000 - $243,000 a year',
+      description: 'We’re looking to hire a Wordpress developer to help optimize the functionality and design of our blog. Position based out of our Los Angeles office, remote work…'
+  )
+
